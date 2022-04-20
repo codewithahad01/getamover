aeaaeec8708b054c41d60a8893149fcd2d53a26e
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-04-21 00:42:12 +0500

    content customized with tailwincss

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,7 @@
 import React from "react";
-import image from '../assets/wallpaper.jpg'
+import image from '../assets/two.jpg'
+// import forwardBtn from '../assets/next.png'
+import checkIcom from '../assets/check.png'
 
 
 const Content = () => {
@@ -10,17 +12,22 @@ const Content = () => {
         <div className="bg-green-200 p-8 m-10 ">
             <form action="#">
                 <input className="px-10 py-3 p-5 outline-gray-300 mr-2" type='text'placeholder="Your Postcode" />
-                <button className="bg-green-500 font-bold px-20 py-3 text-white">Go</button>
+                <button className="bg-green-500 font-bold px-20 py-3 text-white">
+                {/* <img className="w-5 h-5" src={forwardBtn} alt='forward Button' /> */}
+                Go</button>
             </form>
         </div>
-            <ol className="text-lg">
-                <li>Discover your ideal mover with ease</li>
-                <li>Access verified reviews</li>
-                <li>Make your move worry-free!</li>
+            <ol className="text-lg grid grid-cols-2 my-10">
+                <img className="w-6" src={checkIcom} alt="check Icon"/>
+                <li> Discover your ideal mover with ease</li>
+                <img className="w-6" src={checkIcom} alt="check Icon"/>
+                <li> Access verified reviews</li>
+                <img className="w-6" src={checkIcom} alt="check Icon"/>
+                <li> Make your move worry-free!</li>
             </ol>
         
-        <div className="grid justify-items-end ">
-            <img className='w-40 h-40' src={image} alt='logo2' />
+        <div className="grid justify-items-end">
+            <img className=' pl-10' src={image} alt='logo2' />
         </div>
     </div>
     )
