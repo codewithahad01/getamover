0e84aabd481b428645f701c13bfb33254b6529d6
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-04-21 23:49:54 +0500

    inline elements added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,19 +3,41 @@ import logo from '../assets/check.png'
 
 const Card = () => {
     return (
-        <div>
+        <div className="mt-15 text-md p-24">
             <h1 className="text-2xl">Check out our verified reviews</h1>
             <p>On average, our moving companies are rated <span className="bg-green-100 font-bold">8,6 out of 10.</span> Below are some of our<br />
             most recent reviews. Get access to companies by requesting your quote.</p>
             <div>
-                <div>
+                <div className="box-border grid grid-cols-2 p-10 pt-15 p">
                     <h1>"Great Service"</h1>
                     <p>Kyle, 21 hours ago</p>
                     <p>lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
                     <div><a href="#new">
-                        <div><img src={logo} alt="images" /></div>
+                        <div className="w-6 h-6"><img src={logo} alt="images" /></div>
                         <div><p>DS Removals</p></div>
-                        <div></div>
+                        <div><p>9.4</p></div>
+                        </a>
+                    </div>
+                </div>
+                <div className="box-border grid grid-cols-2 p-10 pt-15">
+                    <h1>"Great Service"</h1>
+                    <p>Kyle, 21 hours ago</p>
+                    <p>lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
+                    <div><a href="#new">
+                        <div className="w-6 h-6"><img src={logo} alt="images" /></div>
+                        <div><p>DS Removals</p></div>
+                        <div><p>9.4</p></div>
+                        </a>
+                    </div>
+                </div>
+                <div className="box-border grid grid-cols-2 p-10 pt-15">
+                    <h1>"Great Service"</h1>
+                    <p>Kyle, 21 hours ago</p>
+                    <p>lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
+                    <div><a href="#new">
+                        <div className="w-6 h-6"><img src={logo} alt="images" /></div>
+                        <div><p>DS Removals</p></div>
+                        <div><p>9.4</p></div>
                         </a>
                     </div>
                 </div>
