c491b1b6da5edd8dc67a12abc41c0182baaf2835
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-04-23 20:51:45 +0500

    card added and some style also added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,7 @@ import logo from '../assets/check.png'
 
 const Card = () => {
     return (
-        <div className="mt-15 text-md p-24">
+        <div className="mt-15 text-md p-24 text-center">
         <div className="px-40 ">
             <h1 className="text-4xl font-bold py-6">Check out our verified reviews</h1>
             <p className="text-lg">On average, our moving companies are rated <span className="bg-green-100 font-bold">8,6 out of 10.</span> Below are some of our<br />
@@ -11,10 +11,10 @@ const Card = () => {
         </div>
             <div className="flex justify-center">
                 <div className="flex flex-col justify-center max-w-xs bg-white shadow-xl p-10 pt-15 px-20 m-20">
-                    <h1 className="font-bold">"Great Service"</h1>
-                    <p>Kyle, 21 hours ago</p>
-                    <p>lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
-                    <div><a href="#new">
+                    <h1 className="font-bold leading-loose ">"Great Service"</h1>
+                    <p className="font-mono">Kyle, 21 hours ago</p>
+                    <p className="">lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
+                    <div className="flex items-start"><a href="#new">
                         <div className="w-6 h-6"><img src={logo} alt="images" /></div>
                         <div><p>DS Removals</p></div>
                         <div><p>9.4</p></div>
@@ -23,7 +23,7 @@ const Card = () => {
                 </div>
                 <div className="flex flex-col justify-center max-w-xs bg-white shadow-xl p-10 pt-15 px-20 m-20">
                     <h1 className="font-bold">"Great Service"</h1>
-                    <p>Kyle, 21 hours ago</p>
+                    <p className="font-mono">Kyle, 21 hours ago</p>
                     <p>lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
                     <div><a href="#new">
                         <div className="w-6 h-6"><img src={logo} alt="images" /></div>
@@ -34,7 +34,7 @@ const Card = () => {
                 </div>
                 <div className="flex flex-col justify-center max-w-xs bg-white shadow-xl p-10 pt-15 px-20 m-20">
                     <h1>"Great Service"</h1>
-                    <p>Kyle, 21 hours ago</p>
+                    <p className="font-mono">Kyle, 21 hours ago</p>
                     <p>lerom ipsum is the free<be /> and open source text and<be /> you can use them every</p>
                     <div><a href="#new">
                         <div className="w-6 h-6"><img src={logo} alt="images" /></div>
