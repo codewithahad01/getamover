a8ba21d042848a2f8f9103c07edcc2c2368d2541
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-04-23 21:02:53 +0500

    contens section added

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React from "react";
 import image from '../assets/two.jpg'
+import {FaArrowRight} from 'react-icons/fa'
 
 
 const Content = () => {
@@ -12,12 +13,12 @@ const Content = () => {
                 <div className="bg-green-200 py-8 px-14 m-10 ">
                     <form action="#">
                         <input className="px-10 py-3 p-5 rounded-md outline-gray-500 mr-2" type='text'placeholder="Your Postcode" />
-                        <button className="bg-green-500 font-bold rounded-md px-20 py-3 text-white hover:bg-green-700">
-                        {/* <img className="w-5 h-5" src={forwardBtn} alt='forward Button' /> */}
-                        Go</button>
+                        <button className="bg-green-500 font-bold rounded-md px-20 py-4 text-white hover:bg-green-700">
+                            <FaArrowRight />
+                        </button>
                     </form>
                     </div>
-                    <ol className="text-lg grid grid-cols-1">
+                    <ol className="text-lg grid grid-cols-1 mx-40">
                         <li>Discover your ideal mover with ease</li>
                         <li>Access verified reviews</li>
                         <li>Make your move worry-free!</li>
